88d5523560ab10b71d193cd0eccbe6ed9ba203b9
mijin92 <primrose5560@gmail.com>
2024-10-25 13:12:07 +0900

    Revert "first commit"
    This reverts commit 9191fb44cbb4bffa110c0d267c1e41cf9804e1a3.

--- src/main/java/com/brandlogo/java_react/logo/repository/ImageExampleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.brandlogo.java_react.logo.repository;
-
-import com.brandlogo.java_react.logo.model.ImageExample;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageExampleRepository extends JpaRepository<ImageExample,Long> {
-}
